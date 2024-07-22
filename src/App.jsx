550aa024720baf4717423acdfe4b7cf7e8ce606e
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-07-22 15:02:48 +0100

    feature: setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_COOL_TEST);
+  return (
+    <>
+      
+      <h1>Cool Test</h1>
+       
+    </>
+  );
+}
+
+export default App
