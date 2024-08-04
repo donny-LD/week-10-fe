91a3ca449357895f344019acfe20853a7c249885
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-08-04 05:41:07 +0100

    feature:setup pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   console.log(import.meta.env.VITE_COOL_TEST);
   return (
     <>
-      
       <h1>Cool Test</h1>
-       
     </>
   );
 }
 
-export default App
+export default App;
